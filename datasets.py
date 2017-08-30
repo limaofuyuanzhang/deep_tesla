bf8e45931ac880b2f5ef9556d71067a0796fcd7d
林涛 <13902220743@139.com>
2017-08-30 16:47:26 +0800

    实现英伟达模型和VGG16模型

--- datasets.py
+++ datasets.py
@@ -15,42 +15,33 @@ def load_data():
     cap = cv2.VideoCapture('./epochs/epoch01_front.mkv')
     ret, img = cap.read()
 
-    for i in range(1, 2):
+    for i in range(1, 10):
         # 读取图片
         pathX = './epochs/epoch0' + str(i) + '_front.mkv'
         cap = cv2.VideoCapture(pathX)
+
         while True:
             ret, img = cap.read()
             if(ret):
-                x_train.append(img)
+                x_train.append((img_pre_process(img)))
             else:
                 break
         cap.release()
 
-        # for i in range(10):
-        #     ret, img = cap.read()
-        #     if (ret):
-        #         x_train.append(img)
-        #     else:
-        #         break
-        # cap.release()
-
         # 读取转向角度
         pathY = './epochs/epoch0' + str(i) + '_steering.csv'
         wheel_sig = pd.read_csv(pathY)
-        y_train.append(wheel_sig['wheel'])
+        y_train.extend(wheel_sig['wheel'].values)
 
     x_train = np.array(x_train)
-    print(len(y_train[0]))
-    y_train = np.array(y_train).reshape((len(y_train[0])))
-    print(y_train.shape)
+    y_train = np.array(y_train)
 
     pathX = './epochs/epoch10_front.mkv'
     cap = cv2.VideoCapture(pathX)
     while True:
         ret, img = cap.read()
         if (ret):
-            x_test.append(img)
+            x_test.append(img_pre_process(img))
         else:
             break
     cap.release()
@@ -63,4 +54,19 @@ def load_data():
 
     return (x_train,y_train),(x_test,y_test)
 
-(X_raw, y_raw), (X_raw_test, y_raw_test) = load_data()
+def img_pre_process(img):
+    """
+    Processes the image and returns it
+    :param img: The image to be processed
+    :return: Returns the processed image
+    """
+    ## Chop off 1/3 from the top and cut bottom 150px(which contains the head of car)
+    shape = img.shape
+    img = img[int(shape[0]/3):shape[0]-150, 0:shape[1]]
+    ## Resize the image
+    img = cv2.resize(img, (params.FLAGS.img_w, params.FLAGS.img_h), interpolation=cv2.INTER_AREA)
+    ## Return the image sized as a 4D array
+    return np.resize(img, (params.FLAGS.img_w, params.FLAGS.img_h, params.FLAGS.img_c))
+
+(X_train, y_train), (X_test, y_test) = load_data()
+n_train, n_test = X_train.shape[0], X_test.shape[0]
